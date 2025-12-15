94878564270281a762c64c89dffaa269db8531b5
harrismalik <harrismalik4647@gmail.com>
2025-12-15 21:58:17 +0500

    update

--- src/components/Work/index.tsx
+++ src/components/Work/index.tsx
@@ -22,7 +22,7 @@ export default function Work() {
         <section className="section">
             <div className="container">
                 <h2 className="reveal headline-2 mb-8">
-                    Fun Projects
+                    Fun Projects <br/><span className='italic text-sm'>( this website is under development, adding fun project soon..! )</span>
                 </h2>
                 <div className="grid gap-x-4 gap-y-5 grid-cols-[repeat(auto-fill,_minmax(280px,_1fr))]">
                     {
